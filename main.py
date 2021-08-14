31d2f5223d05fec0ff0bd773416a8eda6890efb0
VasiliyMatlab <vasiliy0399@yandex.ru>
2021-08-14 21:53:56 +0300

    Расстановка мин
    https://www.youtube.com/watch?v=1TdtlronS2A&list=PLQAt0m1f9OHtfXxDph-MJvYCLaOvildGQ&index=5
    - добавил механизм расстановки мин на поле

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
+import random
 import tkinter as tk
+from random import shuffle
 
 class MyButton(tk.Button):
     BUTTON_WIDTH = 3 # размер кнопки
     FONT = "Calibri 15 bold" # шрифт
 
-    def __init__(self, master, x, y, *args, **kwargs) -> None:
+    def __init__(self, master, x, y, number, *args, **kwargs) -> None:
         super(MyButton, self).__init__(master, width=MyButton.BUTTON_WIDTH,\
             font=MyButton.FONT, *args, **kwargs)
         self.x = x
         self.y = y
+        self.number = number
         self.is_mine = False
 
     def __repr__(self) -> str:
-        return f"MyButton{{{self.x}, {self.y}}}"
+        return f"MyButton {self.number} {self.is_mine} {{{self.x}, {self.y}}}"
 
 
 class MineSweeper:
-    window = tk.Tk() # создание окна
-    ROWS    = 10 # кол-во строк
-    COLUMNS = 7  # кол-во столбцов
+    window = tk.Tk()    # создание окна
+    ROWS    = 5         # кол-во строк
+    COLUMNS = 5         # кол-во столбцов
+    MINES  = 15         # кол-во мин
 
     def __init__(self) -> None:
         # Создаем кнопки
         self.buttons = list()
+        count = 1
         # Цикл по строкам
         for i in range(MineSweeper.ROWS):
             temp = list()
             # Цикл по столбцам
             for j in range(MineSweeper.COLUMNS):
-                btn = MyButton(MineSweeper.window, x=i, y=j)
+                btn = MyButton(MineSweeper.window, x=i, y=j, number=count)
                 temp.append(btn)
+                count += 1
             self.buttons.append(temp)
 
     # Создание кнопок
@@ -44,6 +50,7 @@ class MineSweeper:
     # Старт игры
     def start(self):
         self.create_widgets()
+        self.insert_mines()
         self.print_buttons()
         MineSweeper.window.mainloop()
     
@@ -52,6 +59,20 @@ class MineSweeper:
         for row in self.buttons:
             print(row)
 
+    # Расстановка мин
+    def insert_mines(self):
+        mines_indexes = self.get_mines_places()
+        for row in self.buttons:
+            for btn in row:
+                if btn.number in mines_indexes:
+                    btn.is_mine = True
+
+    # Генерация расположения мин
+    @staticmethod
+    def get_mines_places():
+        indexes = list(range(1, MineSweeper.ROWS*MineSweeper.COLUMNS + 1))
+        shuffle(indexes)
+        return indexes[:MineSweeper.MINES]
 
 game = MineSweeper()
 game.start()
